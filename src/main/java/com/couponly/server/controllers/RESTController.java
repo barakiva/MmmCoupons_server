f3f4c7475e9504d42263ababb26ae8d1367ea57d
bar <barakiva1@gmail.com>
2020-06-28 11:44:28 +0300

    about to mock response

--- src/main/java/com/couponly/server/controllers/RESTController.java
+++ src/main/java/com/couponly/server/controllers/RESTController.java
@@ -1,21 +1,15 @@
 package com.couponly.server.controllers;
 
-import ch.qos.logback.core.hook.DelayingShutdownHook;
-import com.couponly.server.model.Coordinates;
 import com.couponly.server.model.Deal;
 import com.couponly.server.model.DealWrapper;
 import com.couponly.server.model.RawResponse;
 import com.couponly.server.services.RequestService;
-import org.apache.http.NameValuePair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -32,7 +26,6 @@ public class RESTController {
     @GetMapping(value = "/test", produces = "application/json")
     public RawResponse getRawResponse() {
         RawResponse response = requestService.requestRawResponse();
-//        logger.info(String.valueOf(response));
         return response;
     }
     @GetMapping(value = "/get-all-deals", produces = "application/json")
@@ -40,16 +33,11 @@ public class RESTController {
         List<DealWrapper> deals = requestService.requestAllDeals("").subList(0,8);
         return dealUnwrap(deals);
     }
-//    @GetMapping(value = "/deals-by-location", produces = "application/json")
-//    public void getDealsByLocation(@RequestParam double latitude, @RequestParam double longtitude) {
-//        Coordinates coor = new Coordinates(latitude, longtitude);
-//        logger.info(Double.toString(coor.getLatitude()));
-//    }
+
     @GetMapping(value = "/deals-by-location", produces = "application/json")
     @ResponseBody
     public ResponseEntity<List<DealWrapper>> getDealsByLocation(@RequestParam String locationName) {
         List<DealWrapper> deals = requestService.requestDealsByLocation(locationName);
-//        logger.info(Double.toString(coors.getLatitude()));
         return new ResponseEntity(deals,HttpStatus.OK);
     }
 
